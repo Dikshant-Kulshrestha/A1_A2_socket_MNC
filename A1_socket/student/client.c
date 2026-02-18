ab2a16f1208c1ffc04a64fd6549c60fb6666b057
Dikshant Kulshrestha <dik29kul@gmail.com>
2026-02-18 14:26:58 -0500

    created socket

--- A1_socket/student/client.c
+++ A1_socket/student/client.c
@@ -8,6 +8,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define BUF_SIZE 4096
+
+
 static void usage(const char *prog) {
     fprintf(stderr, "Usage: %s <server-ip> <server-port>\n", prog);
 }
@@ -25,17 +28,72 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    int port = (int)port_long;
+
     // TODO: Create a TCP socket (AF_INET, SOCK_STREAM).
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+
+       struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        close(sockfd);
+        return 1;
+    }
+
     // TODO: Populate sockaddr_in with server IP/port.
     // TODO: Connect to the server.
 
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("connect");
+        close(sockfd);
+        return 1;
+    }
+
     // TODO: Read from stdin in a loop (read()) and send in chunks.
+
     // TODO: For each chunk, send the *exact bytes* you read.
     //   - Use send()/write() in a loop to handle partial sends.
     //   - Do NOT add newlines, prefixes, or other formatting.
     // The test harness compares server stdout byte-for-byte with stdin input.
 
+    char buf[BUF_SIZE];
+    ssize_t nread;
+
+    while ((nread = read(STDIN_FILENO, buf, sizeof(buf))) != 0) {
+        if (nread < 0) {
+            if (errno == EINTR) continue; // interrupted, retry
+            perror("read");
+            break;
+        }
+
+        ssize_t total_sent = 0;
+        while (total_sent < nread) {
+            ssize_t nsent = send(sockfd, buf + total_sent, nread - total_sent, 0);
+            if (nsent < 0) {
+                if (errno == EINTR) continue; // retry
+                perror("send");
+                close(sockfd);
+                return 1;
+            }
+            total_sent += nsent;
+        }
+    }
+
+
     // TODO: Close the socket before exiting.
+
+    close(sockfd);
 
     return 0;
 }
--- A1_socket/student/server.c
+++ A1_socket/student/server.c
@@ -8,6 +8,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define BUF_SIZE 4096
+
+
 static void usage(const char *prog) {
     fprintf(stderr, "Usage: %s <listen-port>\n", prog);
 }
@@ -25,10 +28,46 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    int port = (int)port_long;
+
+
     // TODO: Create a TCP listen socket (AF_INET, SOCK_STREAM).
+
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd < 0) {
+        perror("socket");
+        return 1;
+    }
     // TODO: Set SO_REUSEADDR on the listen socket.
+
+    int opt = 1;
+    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt");
+        close(listen_fd);
+        return 1;
+    }
+
     // TODO: Bind the socket to INADDR_ANY and the given port.
+
+     struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+
+    if (bind(listen_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("bind");
+        close(listen_fd);
+        return 1;
+    }
+
     // TODO: Listen with a small backlog (e.g., 5-10).
+
+    if (listen(listen_fd, 5) < 0) {
+        perror("listen");
+        close(listen_fd);
+        return 1;
+    }
 
     // TODO: Accept clients in an infinite loop.
     //   - For each client, read in chunks until EOF.
@@ -36,9 +75,49 @@ int main(int argc, char *argv[]) {
     //     Use write(STDOUT_FILENO, ...) in a loop to handle partial writes.
     //   - Do NOT use printf/fputs or add separators/newlines/prefixes.
     //   - The test harness compares server stdout byte-for-byte with client input.
+
+        while (1) {
+        struct sockaddr_in client_addr;
+        socklen_t addrlen = sizeof(client_addr);
+        int client_fd;
+
+        while ((client_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &addrlen)) < 0) {
+            if (errno == EINTR) continue; // interrupted, retry
+            perror("accept");
+            close(listen_fd);
+            return 1;
+        }
+
+        // --- Read from client and write to stdout ---
+
+            char buf[BUF_SIZE];
+        ssize_t nread;
+        while ((nread = read(client_fd, buf, sizeof(buf))) != 0) {
+            if (nread < 0) {
+                if (errno == EINTR) continue; // retry
+                perror("read");
+                break;
+            }
+
+            ssize_t total_written = 0;
+            while (total_written < nread) {
+                ssize_t nwritten = write(STDOUT_FILENO, buf + total_written, nread - total_written);
+                if (nwritten < 0) {
+                    if (errno == EINTR) continue; // retry
+                    perror("write");
+                    break;
+                }
+                total_written += nwritten;
+            }
+        }
+
+        close(client_fd); // close this client
+    }
     // TODO: Handle EINTR and other error cases as specified.
 
     // TODO: Close the listen socket before exiting.
+
+    close(listen_fd);
 
     return 0;
 }
